0b46ae3cbcc121f09cf378ed14054c22e51e76c1
Brian Mickel <brianemickel@berkeley.edu>
2017-01-08 17:38:47 -0500

    added rank functionality to game board object

--- camelCupGame.py
+++ camelCupGame.py
@@ -9,13 +9,13 @@ class GameBoard(object):
         #16 spots for game play plus one spot for winner
         for i in range(17):
             self.spots.append([])
-        self.oasisIndexes = []
-        self.desertIndexes = []
+        self.oasisIndexes = [99]
+        self.desertIndexes = [99]
         self.spots[1] = ['yellow','green','white']
         self.spots[3] = ['orange','blue']
 
-        self.runOutcomesSpots = copy.deepcopy(self.spots)
-        self.runOutcomesAlreadyRolled = {"orange":False,"yellow":False,"green":False,"blue":False,"white":False}
+        # self.runOutcomesSpots = copy.deepcopy(self.spots)
+        # self.runOutcomesAlreadyRolled = {"orange":False,"yellow":False,"green":False,"blue":False,"white":False}
 
         self.alreadyRolled = {"orange":False,"yellow":False,"green":False,"blue":False,"white":False}
 
@@ -23,39 +23,42 @@ class GameBoard(object):
         initSpotIndexes = next(((i, color.index(pieceColor)) for i, color in enumerate(self.spots) if pieceColor in color), None)
         return initSpotIndexes
 
-    def moveGamePiece(self,pieceColor,rollAmount,gameType):
+    def moveGamePiece(self,pieceColor,rollAmount):
         #gameType True if normal move; false if runOutcome move
-        if gameType:
-            spotsInUse = self.spots
-            rolledInUse = self.alreadyRolled
-        else:
-            spotsInUse = self.runOutcomesSpots
-            rolledInUse = self.runOutcomesAlreadyRolled
+        # if gameType:
+        #     spotsInUse = self.spots
+        #     rolledInUse = self.alreadyRolled
+        # elif not gameType:
+        #     spotsInUse = self.runOutcomesSpots
+        #     rolledInUse = self.runOutcomesAlreadyRolled
+        spotsInUse = self.spots
+        rolledInUse = self.alreadyRolled
+        #print(spotsInUse)
 
-        self.alreadyRolled[pieceColor] = True
+        rolledInUse[pieceColor] = True
         # get current location
         initSpotIndexes = self.findCurrentLocationOfPiece(pieceColor)
         # save piece + camels on back to temp variable
         a = initSpotIndexes[0]
         b = initSpotIndexes[1]
-        camelStack = self.spots[a][b:]
+        camelStack = spotsInUse[a][b:]
         # remove elements from initial spot list
         for camel in camelStack:
-            self.spots[a].remove(camel)
+            spotsInUse[a].remove(camel)
 
         # add elements to final spot list
         newSpotIndex = a + rollAmount
         if newSpotIndex > 15:
             print("Game Has Been Won")
-            self.spots[16].extend(camelStack)
+            spotsInUse[16].extend(camelStack)
         else:
             if newSpotIndex in self.oasisIndexes:
-                self.spots[newSpotIndex+1].extend(camelStack)
+                spotsInUse[newSpotIndex+1].extend(camelStack)
             elif newSpotIndex in self.desertIndexes:
-                camelStack.extend(self.spots[newSpotIndex-1])
-                self.spots[newSpotIndex-1] = camelStack
+                camelStack.extend(spotsInUse[newSpotIndex-1])
+                spotsInUse[newSpotIndex-1] = camelStack
             else:
-                self.spots[newSpotIndex].extend(camelStack)
+                spotsInUse[newSpotIndex].extend(camelStack)
 
     def moveGameCard(self, cardType,spotIndex):
         # cardType is "desert" or "oasis"
@@ -72,7 +75,7 @@ class GameBoard(object):
         allColors = ["orange","yellow","green","blue","white"]
         piecesInPlay = []
         for element in allColors:
-            if self.alreadyRolled[element]:
+            if not self.alreadyRolled[element]:
                 piecesInPlay.append(element)
         allOrderOfColors = list(itertools.permutations(piecesInPlay))
         #generates [(0, 1, 2, 3, 4), (0, 1, 2, 4, 3), (0, 1, 3, 2, 4), (0, 1, 3, 4, 2), (0, 1, 4, 2, 3), (0, 1, 4, 3, 2)...]
@@ -84,27 +87,31 @@ class GameBoard(object):
         #allRolls = [(1, 2, 3, 1, 2), (1, 2, 3, 1, 3), (1, 2, 3, 1, 1), (1, 2, 3, 1, 2)]
         return allOrderOfColors, allRolls
 
-    def runMovesAndRecordBoards(self, allOrderOfColors, allRolls):
+    def runMovesAndRecordBoards(self):
+        import copy
+        allOrderOfColors, allRolls = self.generateMoves()
+        #print(allOrderOfColors,allRolls)
         #input Moves
-        allBoardStates = []
+        self.allBoardStates = []
         q = 0
         for orderElement in allOrderOfColors:
             for rollElement in allRolls:
-                self.runOutcomesSpots = copy.deepcopy(self.spots)
+                local_board = GameBoard()
+                local_board.spots = copy.deepcopy(self.spots)
+                local_board.alreadyRolled = self.alreadyRolled
+                local_board.oasisIndexes = self.oasisIndexes
+                local_board.desertIndexes = self.desertIndexes
                 for i in range(len(rollElement)):
-                    self.runOutcomesSpots = moveAPiece(orderElement[i],rollElement[i],False)
+                    local_board.moveGamePiece(orderElement[i],rollElement[i])
                 q += 1
                 # print("local board")
                 # print(local_game_board)
-                allBoardStates.append(copy.deepcopy(local_game_board))
+                self.allBoardStates.append(copy.deepcopy(local_board.spots))
                 # print("all boards")
                 # print(allBoardStates)
             print("%d possibilities calculated" % q)
-        #output List of game boardState
-        return allBoardStates
 
-
-    def flattenBoard(list_of_lists):
+    def flattenBoard(self, list_of_lists):
         #http://stackoverflow.com/questions/11264684/flatten-list-of-lists
         flattened = []
         for sublist in list_of_lists:
@@ -112,23 +119,23 @@ class GameBoard(object):
                 flattened.append(val)
         return flattened
 
-    def getRank(allBoardStates):
+    def getRank(self):
+        import copy
         #flatten List of lists
         listOfRankedLists=[]
-        for j in range(len(allBoardStates)):
-            game_board = allBoardStates[j]
-            localFlattened = flattenBoard(game_board)
+        for j in range(len(self.allBoardStates)):
+            localFlattened = self.flattenBoard(self.allBoardStates[j])
             localFlattened.reverse()
             listOfRankedLists.append(localFlattened)
-        return listOfRankedLists
+        self.listOfRankedLists = copy.deepcopy(listOfRankedLists)
 
-    def summarizeRanks(listOfRankedLists):
-        firstPositionList = [item[0] for item in listOfRankedLists]
+    def summarizeRanks(self):
+        firstPositionList = [item[0] for item in self.listOfRankedLists]
         firstCounter = collections.Counter(firstPositionList)
         print("First Place:")
         print(firstCounter)
 
-        secondPositionList = [item[1] for item in listOfRankedLists]
+        secondPositionList = [item[1] for item in self.listOfRankedLists]
         secondCounter = collections.Counter(secondPositionList)
         print("And Second Place:")
         print(secondCounter)
